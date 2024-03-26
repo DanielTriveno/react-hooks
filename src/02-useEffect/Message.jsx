17cf104f5347996ca0437fe169e150e8f6eea4b8
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-03-25 23:23:35 -0500

    Creating a form with custom Hook

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,11 +1,20 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
-    useEffect(() => {
-      console.log("Message Mounted")
+
+    const [coords, setCoords] = useState({x:0,y:0});
     
+    useEffect(() => {
+
+        const onMouseMove = ({x,y}) => {
+            //const coords ={x,y};
+            setCoords({x,y});
+        }
+
+        window.addEventListener('mousemove', onMouseMove);
+        
       return () => {
-        console.log("Message Unmounted")
+        //window.removeEventListener('mousemove', onMouseMove);
       }
     }, [])
     
@@ -14,6 +23,7 @@ export const Message = () => {
   return (
     <>
         <h3>usuario ya existe</h3>
+        { JSON.stringify(coords)} 
     </>
   )
 }
