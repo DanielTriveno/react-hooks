2b996c9dab6a8187b8e3cd62ca389d2e056f7695
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-03-27 15:39:07 -0500

    UseLayoutEffect

--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -1,21 +1,35 @@
 
-import React from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 
 export const PokemonCard = ({id, name, sprites =[] }) => {
+  
+  
+  const iRef = useRef();
+  const [boxSize, setBoxSize] = useState({width:0, height:0})
+
+  useLayoutEffect(() => {
+    const {height, width} = iRef.current.getBoundingClientRect();
+    setBoxSize({height, width});
+                             
+  }, [])
+ 
   return (
-    <section style = {{height:200}}>
+    <>  
+    <section >
       <h2 className='text-capitalize'>#{id} - {name}</h2>
       
       {/* Imagenes*/}
-      <div>
+      <div style = {{display:'flex'}}>
         {
           sprites.map (sprite=>(
 
-            <img key={sprite} src={sprite} alt={name}/>
+            <img ref={iRef} key={sprite} src={sprite} alt={name} />
           ))
         }
       </div>
     
     </section>
+        <code>{JSON.stringify(boxSize)}</code>
+    </>
   )
 }
