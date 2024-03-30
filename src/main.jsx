ede36d2364c6bbb2247e3493672c3cd7f19d392c
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-03-29 23:54:19 -0500

    Introduction to Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -15,9 +15,10 @@ import { TodoApp } from './08-useReducer/TodoApp'
 //import { CallbackHook } from './06-memos/CallbackHook'
 //import { Padre } from './07-tarea-memo/Padre'
 //import './08-useReducer/intro-reducer';
+//import { TodoApp } from './08-useReducer/TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-  <TodoApp/>
+  <MainApp/>
   // </React.StrictMode>,
 )
